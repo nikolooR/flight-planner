e21a70285bb691a3e178c0638fecb830bd460a0c
Nikolajs <nikolajs.rubaniks@inbox.lv>
2023-07-01 01:51:21 +0300

    add empty spring project

---
+++ src/test/java/io/codelx/flightplanner/FlightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codelx.flightplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
